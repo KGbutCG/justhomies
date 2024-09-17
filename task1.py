c63e2d32f8c8ecf2c68c4fb9e6c8f16f76677792
KG <tobigurin@gmail.com>
2024-09-17 17:52:49 +0300

    домашка пайтон.

--- task1.py
+++ task1.py
@@ -1,10 +1,6 @@
-#1st program
-print(5*(9**0.5))
-#2nd program
-print (9.99>9.98 and 1000!=1000.1)
-#3rd program
-print(2*2+2)
-print(2*(2+2))
-print(2*2+2 == 2*(2+2))
-#4th program
-print(int((10*(float('123.456')))%10))
+example='Фантазия'
+print (example[0])
+print (example[-1])
+print (example [4:8])
+print(example[::-1])
+print(example[1::2])
