05e8e60969d76bc97f6b4749a8b65cdd32f64fa9
KG <tobigurin@gmail.com>
2024-09-18 14:04:02 +0300

    Module 1 4

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+# 2
+my_string = input('Поделись буквами:')
+print('Спасибо за буквы, в таком скудном количестве:', len(my_string))
+# 3
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
