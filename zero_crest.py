c0f8ba06ecbe9bc54170a3eb59aacfe1db72aec1
KG <tobigurin@gmail.com>
2024-10-09 00:52:47 +0300

    GAME OF THE YEAR EDITION

---
+++ zero_crest.py
@@ -0,0 +1,72 @@
+def check_winner():
+    if area[0][0] == 'X' and area[0][1] == 'X' and area[0][2] == 'X':
+        return "X"
+    if area [1][0] == "X" and area [1][1] == "X" and area [1][2] == "X":
+        return "X"
+    if area [2][0] == 'X' and area[2][1] == 'X' and area [2][2] == 'X':
+        return "X"
+    if area[0][0] == 'X' and area[1][0] == "X" and area[2][0] == "X":
+        return "X"
+    if area[0][1] == "X" and area [1][1] == "X" and area [2][1] == "X":
+        return "X"
+    if area[0][2] == "X" and area [1][2] == "X" and area [2][2] == "X":
+        return "X"
+    if area [0][0] == "X" and area[1][1] == 'X' and area[2][2] == "X":
+        return '0'
+    if area [0][2] == "X" and area[1][1] == "X" and area[2][0] == "X":
+        return "X"
+    if area[0][0] == '0' and area[0][1] == '0' and area[0][2] == '0':
+        return "0"
+    if area[1][0] == "0" and area[1][1] == "0" and area[1][2] == "0":
+        return "0"
+    if area[2][0] == '0' and area[2][1] == '0' and area[2][2] == '0':
+        return "0"
+    if area[0][0] == '0' and area[1][0] == "0" and area[2][0] == "0":
+        return "0"
+    if area[0][1] == "0" and area[1][1] == "0" and area[2][1] == "0":
+        return "0"
+    if area[0][2] == "0" and area[1][2] == "0" and area[2][2] == "0":
+        return "0"
+    if area[0][0] == "0" and area[1][1] == '0' and area[2][2] == "0":
+        return '0'
+    if area[0][2] == "0" and area[1][1] == "0" and area[2][0] == "0":
+        return "0"
+    return '*'
+
+
+def draw_area():
+    for i in area:
+        print(*i)
+    print()
+
+area = [["*", "*", "*"], ["*", "*", "*"], ["*", "*", "*"]]
+print('привет, первый игрок, здесь крестики нолики')
+print('____________________________________________')
+draw_area()
+for turn in range(1,10):
+    print(f'Turn: {turn}')
+    if turn % 2 == 0:
+        turn_char = '0'
+        print('zeroessss')
+    else :
+        turn_char = 'X'
+        print('christsssss')
+
+    row = int(input('input stroku (1,2,3)')) - 1
+    column = int(input('input colonnu (1,2,3)')) - 1
+    if area[row] [column] == '*':
+        area [row][column] = turn_char
+    else:
+        print('you missed your chance, bruh')
+        draw_area()
+        continue
+    draw_area()
+
+    if check_winner() == 'X':
+        print('XXXpobedacion')
+        break
+    if check_winner() == '0':
+        print('0 is winners number')
+        break
+    if check_winner() == '*' and turn == 9:
+        print('DRAWsers')
