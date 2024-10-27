ccc7c9b75192a2c1e73c9749004d6d55580b4082
KG <tobigurin@gmail.com>
2024-10-27 15:58:40 +0300

    module_5_1.py

---
+++ module_4_pract/module_5/module_5_1.py
@@ -0,0 +1,17 @@
+class House:
+    pass
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('There`s no such floor')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
